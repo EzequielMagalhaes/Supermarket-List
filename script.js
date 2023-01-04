0e9b9d00988a9959d6c8c957b0d8dc0aff9013b7
Ezequiel Magalhaes <zekes_243@hotmail.com>
2023-01-04 13:57:32 -0300

    Atualizando projeto - 04.01.23

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 var itens = [];
-
 document.querySelector('input[type=submit]')
 .addEventListener('click',()=>{
     var nomeProduto = document.querySelector('input[name=nome_produto]');
@@ -9,16 +8,13 @@ document.querySelector('input[type=submit]')
         nome: nomeProduto.value,
         valor: precoProduto.value
     });
-
-
     /* Espelho de como vai ficar, após a concatenação;
     <div class="lista-produtos-single">
         <h3 class="nome-produto">Produto</h3> <!--Produto-->
         <h3 class="preco-produto"><span>Preco</span></h3"> <!--Preço-->
     </div>
     */
-    
-    let listaProdutos = document.querySelector('.lista-produtos')
+    let listaProdutos = document.querySelector('.lista-produtos');
     let soma = 0;
     listaProdutos.innerHTML=''; //Limpar a lista antes de começar.
     itens.map((val)=>{
@@ -37,8 +33,7 @@ document.querySelector('input[type=submit]')
 
     let elementoSoma = document.querySelector('.soma-produto h2');
     elementoSoma.innerHTML = 'R$:'+soma;
-
-}).addEventListener('submit',logSubmit);
+})
 
 document.querySelector('button[name=limpar]')
 .addEventListener('click',()=>{
